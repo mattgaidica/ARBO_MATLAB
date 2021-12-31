26e864604529a5cab5a46beca74a92f26a11490c
Matt Gaidica <matt@gaidi.ca>
2021-12-31 15:19:28 -0500

    New trial vars, convert absoluteTime from ESLO to local

--- Bio-logging/ESLO/Speaker/readSD_SWA.m
+++ Bio-logging/ESLO/Speaker/readSD_SWA.m
@@ -1,23 +1,29 @@
 % note: there is a lot of typing going on to play nice between C and MATLAB
 % i.e. keep values uint32 until adding sign and convert all to double after
-fname = '/Volumes/SWA_REC/00001.BIN';
+fname = '/Volumes/SWA_REC/00013.BIN';
 fid = fopen(fname);
 A = uint32(fread(fid,inf,'uint32'));
 fclose(fid);
 % these come from central device
-trialVars = {'doSham';'dominantFreq';'phaseAngle';'absoluteTime';'msToStim';'targetPhase'};
+trialVars = {};
+trialVars.doSham        = double(A(dataLen + 1));
+trialVars.dominantFreq  = double(A(dataLen + 2));
+trialVars.phaseAngle    = double(A(dataLen + 3));
+trialVars.trialCount    = double(A(dataLen + 4));
+trialVars.absoluteTime  = double(A(dataLen + 5));
+trialVars.msToStim      = double(A(dataLen + 6));
+trialVars.targetPhase   = double(A(dataLen + 7));
 
 doSave = false;
 Fs = 125; % Hz
-dataLen = numel(A) - numel(trialVars);
+dataLen = numel(A) - numel(fieldnames(trialVars));
 t = linspace(0,dataLen/Fs,dataLen);
-for ii = 1:numel(trialVars)
-    trialVars{ii,2} = double(A(dataLen + ii));
-end
 data = A(1:dataLen);
+
+% add EEG Channel
 dataType = bitshift(bitand(data(1),uint32(0xFF000000)),-24);
-trialVars{size(trialVars,1)+1,1} = 'eegChannel';
-trialVars{size(trialVars,1),2} = dataType - 1;
+trialVars.eegChannel    = dataType - 1;
+
 for iData = 1:numel(data)
     data(iData) = bitand(data(iData),uint32(0x00FFFFFF));
     if (bitget(data(iData),24) == 1) % apply sign
@@ -37,26 +43,28 @@ xticks(min(t):max(t)/8:max(t));
 grid on;
 xlabel('Time (s)');
 ylabel('Amplitude (uV)');
-title(sprintf('Recovered SWA (target = %1.1f°)',trialVars{6,2}/1000));
+d = datetime(trialVars.absoluteTime,'ConvertFrom','epochtime','timezone','Etc/UTC');
+title(sprintf('Recovered SWA (target = %1.1f°)\nat %s',trialVars.targetPhase/1000,...
+    datetime(d,'Format','dd-MMM-yyyy HH:mm:ss','timezone','America/Detroit')));
 xline(max(t)/2,'r:','linewidth',2);
 text(max(t)/2,min(ylim),'DETECT\rightarrow','color','r','fontsize',fs,'verticalalignment','bottom','horizontalalignment','right');
 set(gca,'fontsize',fs-2);
-stimIdx = closest(t,max(t)/2+(trialVars{5,2}/1000));
+stimIdx = closest(t,max(t)/2+(trialVars.msToStim/1000));
 xline(t(stimIdx)+0.05,'r-','linewidth',20);
 text(t(stimIdx),max(ylim),'STIM\rightarrow','color','r','fontsize',fs,'verticalalignment','top','horizontalalignment','right');
 
 %%
-L = 2048*2; % this is a double-sided FFT, ESLO is one-sided
-fftData = double(data(1:round(dataLen/2)))';
-y = decimate(fftData,2); % also performed on ESLO
-Y = fft(fftData,L);
-P2 = abs(Y/3.6).^2; % this divisor was empirically found, don't know why it is required
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-close all
-figure;
-plot(P1);
-xlim([1 90]);
+% % % % L = 2048*2; % this is a double-sided FFT, ESLO is one-sided
+% % % % fftData = double(data(1:round(dataLen/2)))';
+% % % % y = decimate(fftData,2); % also performed on ESLO
+% % % % Y = fft(fftData,L);
+% % % % P2 = abs(Y/3.6).^2; % this divisor was empirically found, don't know why it is required
+% % % % P1 = P2(1:L/2+1);
+% % % % P1(2:end-1) = 2*P1(2:end-1);
+% % % % close all
+% % % % figure;
+% % % % plot(P1);
+% % % % xlim([1 90]);
 %%
 
 subplot(212);
