486d44226910fc1f7f09a3882a852bac46c8f168
Matt Gaidica <matt@gaidi.ca>
2022-02-20 15:50:43 -0500

    CMSIS filters are not working as expected

---
+++ Bio-logging/ESLO/Speaker/CMSISFilter.m
@@ -0,0 +1,60 @@
+function CMSISFilter(f1,f2,Fs,data,doPlot)
+% from https://hackernoon.com/fast-iir-filtering-on-arm-cortex-m-with-cmsis-dsp-and-gnu-octave-qk1n3y66
+% order = requested_order/2;
+% fNyquist=fs/2;
+% [z,p,k] = butter(order,[f1 f2]/fNyquist, 'bandpass');
+
+% my own
+[dataFilt,bpd] = bandpass(data,[f1,f2],Fs);
+
+[z,p,k] = zpk(bpd);
+sos = zp2sos(z,p,k);
+% compute biquad coefficients
+coeffs = sos(:,[1 2 3 5 6]);
+% negate a1 and a2 coeffs (CMSIS expects them negated)
+coeffs(:,4) = -coeffs(:,4);
+coeffs(:,5) = -coeffs(:,5);
+% make a linear array of it
+coeffs = coeffs';
+coeffs = coeffs(:);
+% print the coefficients as expected by CMSIS
+clc;
+fprintf('Order: %i\n\n',filtord(bpd));
+coeffStr = sprintf('%1.10ff,\n',coeffs);
+disp(coeffStr(1:end-2));
+
+if doPlot
+    close all
+    ff(600,600,2);
+    subplot(211);
+    t = linspace(0,numel(data)/Fs,numel(data));
+    plot(t,data,'k-','linewidth',2);
+    yyaxis right;
+    plot(t,dataFilt,'r-');
+    set(gca,'ycolor','r');
+    xlim([min(t),max(t)]);
+    xlabel('Time (s)');
+    legend({'Original','Filtered'});
+    title(sprintf('%1.1f-%1.1fHz @ Fs=%1.1f, %i samples',f1,f2,Fs,numel(data)));
+    set(gca,'fontsize',14);
+    grid on;
+    
+    subplot(212);
+    [P,F] = pspectrum(data,Fs);
+    plot(F,P,'k','linewidth',2);
+    yyaxis right;
+    [P,F] = pspectrum(dataFilt,Fs);
+    [locs,pks] = peakseek(P);
+    [v,k] = max(pks);
+    plot(F,P,'r-');
+    hold on;
+    plot(F(locs(k)),v,'r.');
+    set(gca,'ycolor','r');
+    text(F(locs(k)),v,sprintf('  Fc = %1.2fHz',F(locs(k))),'color','r','fontsize',14);
+    xlim([0 f2*3]);
+    xlabel('Freq. (Hz)');
+    set(gca,'fontsize',14);
+    legend({'Original','Filtered'});
+    title('Power Spectrum');
+    grid on;
+end
--- Bio-logging/ESLO/Speaker/writeTrialForCMSIS.m
+++ Bio-logging/ESLO/Speaker/writeTrialForCMSIS.m
@@ -2,15 +2,31 @@
 % review for algo: 358,368 looks mis-timed, 361,367 would be good for exclusion
 % criteria, 369 looks like higher power in lower freq, 
 Rat = 3;
-fname = '/Users/matt/Dropbox (University of Michigan)/Biologging/Database/R0003/SWA Trials/00220.BIN';
+fname = '/Users/matt/Dropbox (University of Michigan)/Biologging/Database/R0003/SWA Trials/00243.BIN';
 
 Fs = 125;
 writePath = '/Users/matt/Documents/Software/ESLO-Libraries/FFT';
 [~,name] = fileparts(fname);
-[trialVars,EEG,~,rawEEG] = extractSWATrial(fname,Fs);
+[trialVars,EEG,t,rawEEG] = extractSWATrial(fname,Fs);
 EEG_detect = rawEEG(1:round(numel(rawEEG)/2));
+t_detect = t(1:round(numel(rawEEG)/2));
 fileID = fopen(fullfile(writePath,sprintf('R%04d_%s.h',Rat,name)),'w');
 fprintf(fileID,'extern int32_t swaBuffer[SWA_LEN * 2] = {');
 fprintf(fileID,'%i,\n',EEG_detect(1:end-1));
 fprintf(fileID,'%i};\n',EEG_detect(end));
 fclose(fileID);
+
+CMSISFilter(0.5,4,Fs,EEG_detect,1)
+
+%%
+colors = jet(128);
+ff(1200,600);
+lns = [];
+for ii = 1:128
+    dataFilt = bandpass(EEG_detect,[0.8 12],Fs);
+    [P,F] = pspectrum(dataFilt(ii:end),Fs);
+    lns(ii) = plot(F,P,'-','color',colors(ii,:));
+    hold on;
+end
+legend([lns(1) lns(end)],{'full 2s','last 1s'});
+xlim([0 20]);
